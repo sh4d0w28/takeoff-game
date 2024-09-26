a94a14312fda0a6af91c2fca307e4f630a5e0945
Maksim Edush <maksim.edush@orangelogic.com>
2024-09-26 08:44:11 +0700

    added tiled projects

---
+++ tiled/planesprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="planesprite" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="assets/planesprite.bmp" width="192" height="96"/>
+</tileset>
