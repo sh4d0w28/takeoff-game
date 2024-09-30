a358707337168d58f3f0a5f28adde3908bf8ca20
Maksim Edush <maksim.edush@orangelogic.com>
2024-09-30 13:22:37 +0700

    move layer draw to separate classes

---
+++ take-off/src/components/PlaneDisplayUtil.ts
@@ -0,0 +1,124 @@
+import { DirectionEnum } from "../../../common/Enums";
+
+
+export default class PlaneDisplayUtil {
+
+    private static readonly PLANES_SPRITESHEET = 'planesSpriteSheet';
+
+    /**
+     * Draw all the planes using current data as well as current state
+     * 
+     * @param tSize - texture size ( currently 32 )
+     * @param w     - width of canvas ( for calculate starting drawing point of airfield ) 
+     * @param h     - height of canvas ( for calculate starting drawing point of airfield )
+     * @param scene - current scene to draw onto 
+     * @param state - current state recevied from Colyseus
+    */
+    public static drawPlanes(tSize: number, w:number, h:number, scene: Phaser.Scene, state: any) 
+    {
+        // center point based 
+        var fieldLeftX = (w - state.columns * tSize) / 2; 
+        var fieldTopY = (h - state.rows * tSize) / 2;
+
+        // prepare 'planes' object to keep planes data locally for processing
+        if(!scene.data.get('planes')) {
+            scene.data.set('planes', {});
+        }
+
+        let planes = scene.data.get('planes');
+        
+        // for each plane:
+        // either create a sprite object in memory, 
+        // or update coordinates / states / speed / rotation 
+        state.planes.entries().forEach(([sessionId,planeSpec]:any)=>{
+
+            // create image on default position
+            if (!planes[sessionId]) {
+                planes[sessionId] = scene.add.image(0,0,this.PLANES_SPRITESHEET, planeSpec.color).setDepth(2);
+            }
+            var psprite: Phaser.GameObjects.Image = planes[sessionId];
+            // move sprite according to plane state
+            this.drawSinglePlane(fieldLeftX + planeSpec.x * tSize, fieldTopY + planeSpec.y * tSize, tSize, planeSpec, psprite);
+        });
+    }
+    
+    /**
+     * Update a single sprite position / angle / state
+     * To define position used geo funcs
+     * 
+     * @param planeLeftX    - left point of plane coords
+     * @param planeTopY     - top point of plane coords
+     * @param tSize texture - texture size ( currently 32 )
+     * @param planeSpec     - plane data from Colyseus
+     * @param sprite        - current plane sprite
+     */
+    private static drawSinglePlane(planeLeftX: integer, planeTopY: integer, tSize:number, planeSpec: any, sprite: Phaser.GameObjects.Image) 
+    {
+        var subModX = 0;
+        var subModY = 0;
+        var angle = 0;
+        if(planeSpec.currentDirection == DirectionEnum.RIGHT && planeSpec.desiredDirection == DirectionEnum.RIGHT) {
+            subModX = -0.5 + planeSpec.subMove;
+            subModY = 0;
+            angle = 90;
+        } else if(planeSpec.currentDirection == DirectionEnum.LEFT && planeSpec.desiredDirection == DirectionEnum.LEFT) {
+            subModX = 0.5 - planeSpec.subMove;
+            subModY = 0;
+            angle = 270;
+        } else if(planeSpec.currentDirection == DirectionEnum.UP && planeSpec.desiredDirection == DirectionEnum.UP) {
+            subModX = 0; 
+            subModY = 0.5 - planeSpec.subMove;
+            angle = 0;
+        } else if(planeSpec.currentDirection == DirectionEnum.DOWN && planeSpec.desiredDirection == DirectionEnum.DOWN) {
+            subModX = 0;
+            subModY = -0.5 + planeSpec.subMove;
+            angle = 180; 
+        } else if ( planeSpec.currentDirection == DirectionEnum.RIGHT && planeSpec.desiredDirection == DirectionEnum.DOWN) {
+            var a = 90 * (planeSpec.subMove);     
+            subModX = -0.5 + 0.5 * Math.cos((270 + a) * Math.PI/180);
+            subModY = 0.5 + 0.5 * Math.sin((270 + a) * Math.PI/180);
+            angle = 90 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.DOWN && planeSpec.desiredDirection == DirectionEnum.LEFT) {
+            var a = 90 * (planeSpec.subMove);     
+            subModX = -0.5 + 0.5 * Math.cos((360 + a) * Math.PI/180);
+            subModY = -0.5 + 0.5 * Math.sin((360 + a) * Math.PI/180);
+            angle = 180 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.LEFT && planeSpec.desiredDirection == DirectionEnum.UP) {
+            var a = 90 * (planeSpec.subMove);     
+            subModX = 0.5 + 0.5 * Math.cos((90 + a) * Math.PI/180);
+            subModY = -0.5 + 0.5 * Math.sin((90 + a) * Math.PI/180);
+            angle = 270 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.UP && planeSpec.desiredDirection == DirectionEnum.RIGHT) {
+            var a = 90 * (planeSpec.subMove);     
+            subModX = 0.5 + 0.5 * Math.cos((180 + a) * Math.PI/180);
+            subModY = 0.5 + 0.5 * Math.sin((180 + a) * Math.PI/180);
+            angle = 0 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.DOWN && planeSpec.desiredDirection == DirectionEnum.RIGHT) {
+            var a = 90 * (-planeSpec.subMove);     
+            subModX = 0.5 + 0.5 * Math.cos((180 + a) * Math.PI/180);
+            subModY = -0.5 + 0.5 * Math.sin((180 + a) * Math.PI/180);
+            angle = 180 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.LEFT && planeSpec.desiredDirection == DirectionEnum.DOWN) {
+            var a = 90 * (-planeSpec.subMove);    
+            subModX = 0.5 + 0.5 * Math.cos((270 + a) * Math.PI/180);
+            subModY = 0.5 + 0.5 * Math.sin((270 + a) * Math.PI/180);
+            angle = 270 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.UP && planeSpec.desiredDirection == DirectionEnum.LEFT) {
+            var a = 90 * (-planeSpec.subMove);
+            subModX = -0.5 + 0.5 * Math.cos((0 + a) * Math.PI/180);
+            subModY = 0.5 + 0.5 * Math.sin((0 + a) * Math.PI/180);
+            angle = 360 + a;
+        } else if ( planeSpec.currentDirection == DirectionEnum.RIGHT && planeSpec.desiredDirection == DirectionEnum.UP) {
+            var a = 90 * (-planeSpec.subMove);
+            subModX = -0.5 + 0.5 * Math.cos((90+a) * Math.PI/180);
+            subModY = -0.5 + 0.5 * Math.sin((90+a) * Math.PI/180);
+            angle = 90 + a;
+        } else {
+            subModX = 0; 
+            subModY = 0;
+        }
+        sprite
+            .setPosition(planeLeftX + subModX * tSize ,planeTopY + subModY * tSize)
+            .setRotation(angle * Math.PI / 180);
+    }
+}
--- take-off/src/scenes/PlayField.ts
+++ take-off/src/scenes/PlayField.ts
@@ -1,7 +1,10 @@
 import { Scene } from 'phaser';
 import GlobalConfig from '../GlobalConfig';
 import { Room } from 'colyseus.js';
-import { DirectionEnum } from '../../../common/Enums';
+import PlaneDisplayUtil from '../components/PlaneDisplayUtil';
+import GroundDisplayUtil from '../components/GroundDisplayUtil';
+import BonusDisplayUtil from '../components/BonusDisplayUtil';
+import PlayerUiDisplayUtil from '../components/PlayerUiDisplayUtil';
 
 export class PlayField extends Scene {   
 
@@ -60,12 +63,20 @@ export class PlayField extends Scene {
         
         var controlBtns = this.input.keyboard?.addKeys('W,S,A,D');
         if(controlBtns) {
-            controlBtns['W'].addListener('down',function(){room.send('wasd', 'w');});
-            controlBtns['A'].addListener('down',function(){room.send('wasd', 'a');});
-            controlBtns['S'].addListener('down',function(){room.send('wasd', 's');});
-            controlBtns['D'].addListener('down',function(){room.send('wasd', 'd');});
+            controlBtns.W.addListener('down',function(){room.send('wasd', 'w');});
+            controlBtns.A.addListener('down',function(){room.send('wasd', 'a');});
+            controlBtns.S.addListener('down',function(){room.send('wasd', 's');});
+            controlBtns.D.addListener('down',function(){room.send('wasd', 'd');});
         }
-
+        room.onMessage("state", (message) => {
+            switch(message) {
+                case 'LOST':
+                    debugger;
+                    break;
+                default:
+                    break;
+            }
+        });
         room.onStateChange((state) => {
             this.data.set("state", state);
             this.data.set('loaded', true);
@@ -80,168 +91,10 @@ export class PlayField extends Scene {
 
         var w = 800;
         var h  = 600;
-        this._drawGroundTiles(w,h, state);
-        this._drawPlanes(w,h, state);
-        this._drawBonuses(w,h, state);
-        this._drawSpeedAndScore(w,h,state);
-    }
 
-    _turn() {
-        var sessionId = this.data.get('GlobalConfig').room.sessionId;
-        
-    }
-
-    _drawSpeedAndScore(w:number, h:number, state:any) {
-        var fieldX = (w - state.columns * this.tsize) / 2 
-        var fieldY = (h - state.rows * this.tsize) / 2
-        var sessionId = this.data.get('GlobalConfig').room.sessionId;
-        
-        if(!this.data.get('score')) {
-            this.data.set('score', this.add.text(fieldX, fieldY-30, 'score').setDepth(3));
-        }
-        var speed  = state.planes.get(sessionId).currentSpeed;
-        let speedCtrl:Phaser.GameObjects.Text = this.data.get('score');
-
-        speedCtrl.setText('speed: ' + state.planes.get(sessionId).currentSpeed + " / " + state.planes.get(sessionId).takeOffSpeed);
-
-    }
-
-    _drawBonuses(w:number, h:number, state: any) {
-        
-        var fieldX = (w - state.columns * this.tsize) / 2 
-        var fieldY = (h - state.rows * this.tsize) / 2
-
-        if(!this.data.get('bonuses')) {
-            this.data.set('bonuses', {});
-        }
-
-        var incomingBKeys:string[] = [];
-        state.bonuses.keys().forEach((key:string) => {
-            incomingBKeys.push(key);
-        });
-
-        state.bonuses.entries().forEach(([id ,bonusSpec]:any)=>{ 
-            if(!this.data.get('bonuses')[id]) {
-                var x = fieldX + bonusSpec.x * this.tsize;
-                var y = fieldY + bonusSpec.y * this.tsize; 
-                this.data.get('bonuses')[id] = this.add.sprite(x, y,'bonusSpriteAnimated',0).setDepth(3);
-                this.data.get('bonuses')[id].anims.play('bonus');
-            }
-        });
-
-        Object.keys(this.data.get("bonuses")).forEach((k:string) => {
-            if (!incomingBKeys.includes(k)) {
-                this.data.get("bonuses")[k].destroy();
-                delete this.data.get("bonuses")[k]; 
-            }
-        });
-    }
-
-    _drawPlanes(w:number, h:number, state: any) {
-        var fieldX = (w - state.columns * this.tsize) / 2; 
-        var fieldY = (h - state.rows * this.tsize) / 2;
-
-        if(!this.data.get('planes')) {
-            this.data.set('planes', {});
-        }
-        state.planes.entries().forEach(([sessionId,planeSpec]:any)=>{
-            if (!this.data.get('planes')[sessionId]) {
-                this.data.get('planes')[sessionId] = this.add.image(0,0,"planesSpriteSheet", planeSpec.color).setDepth(2);
-            }
-           var psprite: Phaser.GameObjects.Image = this.data.get('planes')[sessionId];
-           this.__drawSinglePlane(fieldX, fieldY, planeSpec, psprite);
-        });
-    }
-    __drawSinglePlane(fieldX: integer, fieldY: integer, planeSpec: any, sprite: Phaser.GameObjects.Image) {
-        var subModX = 0;
-        var subModY = 0;
-        var angle = 0;
-        if(planeSpec.currentDirection == DirectionEnum.RIGHT && planeSpec.desiredDirection == DirectionEnum.RIGHT) {
-            subModX = -0.5 + planeSpec.subMove;
-            subModY = 0;
-            angle = 90;
-        } else if(planeSpec.currentDirection == DirectionEnum.LEFT && planeSpec.desiredDirection == DirectionEnum.LEFT) {
-            subModX = 0.5 - planeSpec.subMove;
-            subModY = 0;
-            angle = 270;
-        } else if(planeSpec.currentDirection == DirectionEnum.UP && planeSpec.desiredDirection == DirectionEnum.UP) {
-            subModX = 0; 
-            subModY = 0.5 - planeSpec.subMove;
-            angle = 0;
-        } else if(planeSpec.currentDirection == DirectionEnum.DOWN && planeSpec.desiredDirection == DirectionEnum.DOWN) {
-            subModX = 0;
-            subModY = -0.5 + planeSpec.subMove;
-            angle = 180; 
-        } else if ( planeSpec.currentDirection == DirectionEnum.RIGHT && planeSpec.desiredDirection == DirectionEnum.DOWN) {
-            var a = 90 * (planeSpec.subMove);     
-            subModX = -0.5 + 0.5 * Math.cos((270 + a) * Math.PI/180);
-            subModY = 0.5 + 0.5 * Math.sin((270 + a) * Math.PI/180);
-            angle = 90 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.DOWN && planeSpec.desiredDirection == DirectionEnum.LEFT) {
-            var a = 90 * (planeSpec.subMove);     
-            subModX = -0.5 + 0.5 * Math.cos((360 + a) * Math.PI/180);
-            subModY = -0.5 + 0.5 * Math.sin((360 + a) * Math.PI/180);
-            angle = 180 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.LEFT && planeSpec.desiredDirection == DirectionEnum.UP) {
-            var a = 90 * (planeSpec.subMove);     
-            subModX = 0.5 + 0.5 * Math.cos((90 + a) * Math.PI/180);
-            subModY = -0.5 + 0.5 * Math.sin((90 + a) * Math.PI/180);
-            angle = 270 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.UP && planeSpec.desiredDirection == DirectionEnum.RIGHT) {
-            var a = 90 * (planeSpec.subMove);     
-            subModX = 0.5 + 0.5 * Math.cos((180 + a) * Math.PI/180);
-            subModY = 0.5 + 0.5 * Math.sin((180 + a) * Math.PI/180);
-            angle = 0 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.DOWN && planeSpec.desiredDirection == DirectionEnum.RIGHT) {
-            var a = 90 * (-planeSpec.subMove);     
-            subModX = 0.5 + 0.5 * Math.cos((180 + a) * Math.PI/180);
-            subModY = -0.5 + 0.5 * Math.sin((180 + a) * Math.PI/180);
-            angle = 180 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.LEFT && planeSpec.desiredDirection == DirectionEnum.DOWN) {
-            var a = 90 * (-planeSpec.subMove);    
-            subModX = 0.5 + 0.5 * Math.cos((270 + a) * Math.PI/180);
-            subModY = 0.5 + 0.5 * Math.sin((270 + a) * Math.PI/180);
-            angle = 270 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.UP && planeSpec.desiredDirection == DirectionEnum.LEFT) {
-            var a = 90 * (-planeSpec.subMove);
-            subModX = -0.5 + 0.5 * Math.cos((0 + a) * Math.PI/180);
-            subModY = 0.5 + 0.5 * Math.sin((0 + a) * Math.PI/180);
-            angle = 360 + a;
-        } else if ( planeSpec.currentDirection == DirectionEnum.RIGHT && planeSpec.desiredDirection == DirectionEnum.UP) {
-            var a = 90 * (-planeSpec.subMove);
-            subModX = -0.5 + 0.5 * Math.cos((90+a) * Math.PI/180);
-            subModY = -0.5 + 0.5 * Math.sin((90+a) * Math.PI/180);
-            angle = 90 + a;
-        } else {
-            subModX = 0; 
-            subModY = 0;
-        }
-        sprite.setPosition(fieldX + (planeSpec.x + subModX) * this.tsize ,fieldY + (planeSpec.y + subModY) * this.tsize).setRotation(angle * Math.PI / 180);
-    }
-
-    _drawGroundTiles(w:number,h:number, state: any) {
-        var fieldX = (w - state.columns * this.tsize) / 2 
-        var fieldY = (h - state.rows * this.tsize) / 2
-        
-        state.mapSpecification.forEach((tile:string ,coord:string)=>{
-            const [x,y] = coord.split('.').map(v => parseInt(v));
-            this.__drawMapChar(fieldX + x * this.tsize, fieldY + y * this.tsize, tile);
-        });
-    }
-
-    __drawMapChar(x:integer,y:integer, c: string) {
-        switch(c) {
-            case '╔': case '┌': this.add.image(x,y, "roadSpriteSheet", 2).setRotation( 0         ); break;
-            case '╗': case '┐': this.add.image(x,y, "roadSpriteSheet", 2).setRotation( Math.PI/2 ); break;
-            case '╝': case '┘': this.add.image(x,y, "roadSpriteSheet", 2).setRotation( Math.PI   ); break;
-            case '╚': case '└': this.add.image(x,y, "roadSpriteSheet", 2).setRotation(-Math.PI/2 ); break;
-            case '║': case '│': this.add.image(x,y, "roadSpriteSheet", 1).setRotation( 0         ); break;
-            case '═': case '─': this.add.image(x,y, "roadSpriteSheet", 1).setRotation( Math.PI/2 ); break;
-            case '┤': this.add.image(x,y, "roadSpriteSheet", 0).setRotation( Math.PI   ); break; 
-            case '┴': this.add.image(x,y, "roadSpriteSheet", 0).setRotation(-Math.PI/2 ); break;
-            case '├': this.add.image(x,y, "roadSpriteSheet", 0).setRotation( 0         ); break;
-            case '┬': this.add.image(x,y, "roadSpriteSheet", 0).setRotation( Math.PI/2 ); break;
-            case '┼': this.add.image(x,y, "roadSpriteSheet", 3).setRotation( 0         ); break;
-        }
+        GroundDisplayUtil.drawGroundTiles(this.tsize, w, h, this, state)
+        PlaneDisplayUtil.drawPlanes(this.tsize, w, h, this, state);
+        BonusDisplayUtil.drawBonuses(this.tsize, w, h, this, state);
+        PlayerUiDisplayUtil.drawGUI(this.tsize, w, h, this, state);
     }
 }
