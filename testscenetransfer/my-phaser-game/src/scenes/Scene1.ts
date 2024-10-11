23556c6a7f194e1b6a1c36a55c38cab161467e66
Maksim Edush <maksim.edush@orangelogic.com>
2024-10-11 17:40:08 +0700

    attempt to switch

--- testscenetransfer/my-phaser-game/src/scenes/Scene1.ts
+++ testscenetransfer/my-phaser-game/src/scenes/Scene1.ts
@@ -5,6 +5,7 @@ export class Scene1 extends Scene
     private top:Phaser.GameObjects.Rectangle;
     private left:Phaser.GameObjects.Rectangle;
     private right: Phaser.GameObjects.Rectangle;
+    private leftcontainer: Phaser.GameObjects.Container;
 
 
     constructor ()
@@ -13,31 +14,47 @@ export class Scene1 extends Scene
     }
 
     create ()
-    {   
+    {
+        console.log('create');   
         this.top = this.add.rectangle(20, 20, 760, 60, 0x111111, 0.9).setOrigin(0);
         this.left = this.add.rectangle(20, 100, 512, 480, 0x111111, 0.9).setOrigin(0);
         this.right = this.add.rectangle(552, 100, 228, 480, 0x111111, 0.9).setOrigin(0);
 
-        this._setLayoutFromScene2ToScene1();
+        var text = this.add.text(20,20,"SC1");
+        var text2 = this.add.text(20,40,"SC1-1");
+
+        this.leftcontainer = this.add.container(20,100, [
+            text, text2
+        ]);
+
+        this.moveFromScene1ToScene2();
 
     }
 
-    _setLayoutFromScene2ToScene1() {
+    moveFromScene1ToScene2() {
         let self = this
         this.tweens.chain({
-            tweens:[{
-            targets: this.right,
-            alpha:0,
-            duration: 1000
-            },{
-                targets: this.left,
-                ease: 'Cubic',
-                width: 760,
-                duration: 1000,
-                onComplete: function() {
-                    self.scene.switch('Scene2', {});
+            tweens:[
+                {
+                    targets: this.leftcontainer,
+                    alpha:0,
+                    ease: 'Cubic',
+                    duration: 500
+                },
+                {
+                    targets: this.right,
+                    alpha:0,
+                    ease: 'Cubic',
+                    duration: 300
+                },
+                {
+                    targets: this.left,
+                    ease: 'Cubic',
+                    width: 760,
+                    duration: 1000,
+                    onComplete: function() { self.scene.switch('Scene2', {}); }
                 }
-            }]
+            ]
         });
     }
 }
--- testscenetransfer/my-phaser-game/src/scenes/Scene2.ts
+++ testscenetransfer/my-phaser-game/src/scenes/Scene2.ts
@@ -11,5 +11,6 @@ export class Scene2 extends Scene
     {
         this.add.rectangle(20, 20, 760, 60, 0x111111, 0.9).setOrigin(0);
         this.add.rectangle(20, 100, 760, 480, 0x111111, 0.9).setOrigin(0);
+        this.add.text(100, 100, "SC2-2");
     }
 }
