7e0541f42ef06f3e8e8a585054a4071e353efb01
Maksim Edush <maksim.edush@orangelogic.com>
2024-10-15 15:48:50 +0700

    fixed nineslice. Required single image

--- testscenetransfer/my-phaser-game/src/scenes/Scene1.ts
+++ testscenetransfer/my-phaser-game/src/scenes/Scene1.ts
@@ -1,19 +1,29 @@
-import { Scene, Tweens } from 'phaser';
-import { containerOfGameObject } from '../Utils'
+import { Scene } from 'phaser';
+import { containerOfNineSlice } from '../Utils'
 
 export class Scene1 extends Scene
 {
-    private topRectangle:Phaser.GameObjects.Rectangle;
-    private leftRectangle:Phaser.GameObjects.Rectangle;
-    private rightRectangle: Phaser.GameObjects.Rectangle;
+    private topRectangle:Phaser.GameObjects.NineSlice;
+    private leftRectangle:Phaser.GameObjects.NineSlice;
+    private rightRectangle: Phaser.GameObjects.NineSlice;
     private topContainer: Phaser.GameObjects.Container;
     private leftContainer: Phaser.GameObjects.Container;
     private rightContainer: Phaser.GameObjects.Container;
 
+    private spritesheet_id = 'borderSpreadSheet';
+    private spritesheet_file = 'assets/rect_bg2.png';
 
     constructor ()
     {
         super('Scene1');
+    }
+
+    preload() {
+
+        this.load.image({
+            key: this.spritesheet_id,
+            url: this.spritesheet_file
+        });
     }
 
     create (data: any)
@@ -23,15 +33,15 @@ export class Scene1 extends Scene
         this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE).addListener('down', function() { self.moveFromScene1ToScene2(self); });
 
         // top rectangle is always there
-        this.topRectangle = this.add.rectangle(20, 20, 760, 60, 0x111111, 0.9).setOrigin(0);
+        this.topRectangle = this.add.nineslice(20,20,'borderSpreadSheet', undefined,760,60,32,32,32,32).setOrigin(0).setAlpha(0.9);
 
         if(data.prepareFromScene2ToScene1) {
-            this.leftRectangle = this.add.rectangle(20, 100, 760, 480, 0x111111, 0.9).setOrigin(0);
+            this.leftRectangle = this.add.nineslice(20,100,'borderSpreadSheet', undefined,760,480,32,32,32,32).setOrigin(0).setAlpha(0.9);
         } else {
-            this.leftRectangle = this.add.rectangle(20, 100, 512, 480, 0x111111, 0.9).setOrigin(0);    
+            this.leftRectangle = this.add.nineslice(20,100,'borderSpreadSheet', undefined,512,480,32,32,32,32).setOrigin(0).setAlpha(0.9);
         }
         
-        this.rightRectangle = this.add.rectangle(552, 100, 228, 480, 0x111111, 0.9).setOrigin(0);
+        this.rightRectangle = this.add.nineslice(552,100,'borderSpreadSheet', undefined,228,480,32,32,32,32).setOrigin(0).setAlpha(0.9);
 
         var titleText = this.add.text(20,20,"Main screen");
 
@@ -41,9 +51,9 @@ export class Scene1 extends Scene
         var highScoreTitleLine = this.add.text(20,20,"Score");
         var highScoreEntryLine = this.add.text(20,40,"Score1: 000");
 
-        this.topContainer = containerOfGameObject(this, this.topRectangle, [titleText]);
-        this.leftContainer = containerOfGameObject(this, this.leftRectangle, [menuTextLine1, menuTextLine2]);
-        this.rightContainer = containerOfGameObject(this, this.rightRectangle, [highScoreTitleLine, highScoreEntryLine]);
+        this.topContainer = containerOfNineSlice(this, this.topRectangle, [titleText]);
+        this.leftContainer = containerOfNineSlice(this, this.leftRectangle, [menuTextLine1, menuTextLine2]);
+        this.rightContainer = containerOfNineSlice(this, this.rightRectangle, [highScoreTitleLine, highScoreEntryLine]);
 
         if(data.prepareFromScene2ToScene1) {
             this.completeMoveFromScene2ToScene1();
--- testscenetransfer/my-phaser-game/src/scenes/Scene2.ts
+++ testscenetransfer/my-phaser-game/src/scenes/Scene2.ts
@@ -1,16 +1,26 @@
 import { Scene } from 'phaser';
-import { containerOfGameObject } from '../Utils'
+import { containerOfNineSlice } from '../Utils'
 
 export class Scene2 extends Scene
 {
-    private topRectangle:Phaser.GameObjects.Rectangle;
-    private leftRectangle:Phaser.GameObjects.Rectangle;
+    private topRectangle:Phaser.GameObjects.NineSlice;
+    private leftRectangle:Phaser.GameObjects.NineSlice;
     private topContainer: Phaser.GameObjects.Container;
     private leftContainer: Phaser.GameObjects.Container;
+
+    private spritesheet_id = 'borderSpreadSheet';
+    private spritesheet_file = 'assets/rect_bg2.png';
 
     constructor ()
     {
         super('Scene2');
+    }
+
+    preload() {
+        this.load.image({
+            key: this.spritesheet_id,
+            url: this.spritesheet_file
+        });
     }
 
     create (data: any)
@@ -19,14 +29,17 @@ export class Scene2 extends Scene
         // add space to trigger change
         this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE).addListener('down', function() { self.moveFromScene2ToScene1(self); });
 
-        this.topRectangle = this.add.rectangle(20, 20, 760, 60, 0x111111, 0.9).setOrigin(0);
-        this.leftRectangle = this.add.rectangle(20, 100, 760, 480, 0x111111, 0.9).setOrigin(0);
+        this.topRectangle = this.add.nineslice(20,20,'borderSpreadSheet', undefined,760,60,32,32,32,32).setOrigin(0).setAlpha(0.9);
+        // this.topRectangle = this.add.rectangle(20, 20, 760, 60, 0x111111, 0.9).setOrigin(0);
+  
+        this.leftRectangle = this.add.nineslice(20,100,'borderSpreadSheet', undefined,760,480,32,32,32,32).setOrigin(0).setAlpha(0.9);
+        // this.leftRectangle = this.add.rectangle(20, 100, 760, 480, 0x111111, 0.9).setOrigin(0);
     
         var titleText = this.add.text(20, 20, "Lobby screen");
-        this.topContainer = containerOfGameObject(this, this.topRectangle, [titleText]);
+        this.topContainer = containerOfNineSlice(this, this.topRectangle, [titleText]);
         
         var oneroom = this.add.text(20,20, 'ROOM#1');
-        this.leftContainer = containerOfGameObject(this, this.leftRectangle,[oneroom]);
+        this.leftContainer = containerOfNineSlice(this, this.leftRectangle,[oneroom]);
 
         if(data.prepareFromScene1ToScene2) {
             this.completeMoveFromScene1ToScene2();
