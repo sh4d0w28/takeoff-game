74ec051fb3c062803ea8b21a83fde61c812241ea
Maksim Edush <maksim.edush@orangelogic.com>
2024-10-18 15:53:34 +0700

    remove old data

--- testscenetransfer/my-phaser-game/src/Utils.ts
+++
@@ -1,9 +0,0 @@
-import { GameObjects, Scene } from "phaser";
-
-export function containerOfGameObject(s: Scene, o: GameObjects.Rectangle, kids: GameObjects.GameObject[]) {
-    return s.add.container(o.x, o.y, kids);
-} 
-
-export function containerOfNineSlice(s:Scene, o: GameObjects.NineSlice, kids: GameObjects.GameObject[]) {
-    return s.add.container(o.x, o.y, kids);
-}
